529548da835cd93e821cfe4103f0bfe81fca5334
Mike Szczys <szczys@hotmail.com>
2021-03-18 20:36:31 -0500

    Began development for 5.83" screen

--- readme.md
+++ readme.md
@@ -4,6 +4,16 @@ Play one frame per second of a movie on a ePaper display. This is a wild way to 
 
 This installation uses an ESP32 and a 2.7" ePaper display. Images are passed via MQTT by a server-size script that uses ffmpeg to grab the image and imagemagick to format it.
 
+## Notes on dev583 branch
+
+In development for 5.83" ePaper screen
+
+Oneliner for formatting the frames:
+`convert frame.png -resize "648x480^" -gravity center -crop 648x480+0+0 -dither FloydSteinberg frame.png`
+
+Oneliner for pushing a frame to MQTT:
+`mosquitto_pub -h 192.168.1.135 -t "slowmovie/frame" -f frame.pbm`
+
 ## Crontab
 
 Run this script every minute using the following crontab job:
---
+++ slowmovie-esp32-583/slowmovie-esp32-583.ino
@@ -0,0 +1,190 @@
+/*
+ * slowmovie 
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2020 Mike Szczys
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*
+ * Library dependancies:
+ *   GxEPD
+ *   PubSubClient
+ */
+
+#include <WiFi.h>
+/* Enter your WiFi access point ssid and password in this headerfile
+ * format: 
+ *      const char* ssid = "ssid";
+ *      const char* password = "password";
+ */
+#include "wifi_credentials.h"
+
+const uint8_t header[] = {0x36,0x34,0x38,0x20,0x34,0x38,0x30,0x0a}; //Header for 648x480: "648 480[Linefeed]"
+
+#include <PubSubClient.h>
+const char* mqtt_server = "192.168.1.135";
+/****
+ * IMPORTANT: Packet size must be changed inside the PubSubClient library files
+ * edit: ~/Arduino/libraries/PubSubClient/src/PubSubClient.h
+ * change: #define MQTT_MAX_PACKET_SIZE 12000 
+ */
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[39200];
+uint8_t displayFrame[5808];
+int value = 0;
+
+
+//ESP32 pin definitions
+
+
+//Prototypes
+bool headers_match(uint8_t *slice);
+void shift_header_buf_left(uint8_t *slice);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  //display.init(115200); // enable diagnostic output on Serial
+
+  for (uint16_t i=0; i<5808; i++) { displayFrame[i] = 0; }
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  Serial.println("setup done");
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  /*
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  */
+  Serial.println();
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "slowmovie/frame") {
+    Serial.print("Received a message of length: ");
+
+    uint8_t header_buf[] = {0,0,0,0,0,0,0,0};
+
+    uint8_t data_offset = 0;
+    for (uint8_t i=0; i<200; i++) {
+      //If we don't find header in the first 200 chars, assume something went wrong
+      header_buf[7] = message[i];
+      if (message[i] == 0x0A) {
+        //Line Feed; this is what ends a valid header
+        if (headers_match(header_buf) == 1) {
+          data_offset = i+1;
+          Serial.print("Header found! Data begins at: ");
+          Serial.println(data_offset);
+          break;
+        }
+      }
+      //Shift header_checker left
+      shift_header_buf_left(header_buf);
+    }
+
+    if (data_offset == 0) {
+      Serial.println("Correct header not found. This message is not a properly formatted image");
+    }
+    // for (uint16_t i=0; i<5808; i++) {
+    //   uint8_t hNibble = (char)message[i*2];
+    //   uint8_t lNibble = (char)message[(i*2)+1];
+    //   uint8_t storageByte = 0;
+    //   if (hNibble < 65) { storageByte += (hNibble-48)<<4; }
+    //   else { storageByte += (hNibble-55)<<4; }
+    //   if (lNibble < 65) { storageByte += lNibble-48; }
+    //   else { storageByte += lNibble-55; }
+    //   if (i%100 == 0) { Serial.println(i); }
+    //   displayFrame[i] = storageByte;
+    // }
+    // display.drawExampleBitmap(displayFrame, sizeof(displayFrame));
+  }
+}
+
+bool headers_match(uint8_t *slice) {
+  for (uint8_t i=0; i<8; i++) {
+    if (header[i] != slice[i]) {
+      return 0; //members don't match
+    }
+  }
+  return 1;
+}
+
+void shift_header_buf_left(uint8_t *slice) {
+  for (uint8_t i=1; i<8; i++) {
+    slice[i-1] = slice[i];
+  }
+  slice[7] == 0;
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("slowmovie/frame");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
